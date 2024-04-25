d9f34ba86bdf2ef6c3801d2a42d29d97e80f543f
Matheus Viersa <matheusviersa995@hotmail.com>
2024-04-25 08:32:51 -0300

    Seção planos

--- src/main.js
+++ src/main.js
@@ -1,30 +1,28 @@
-document.addEventListener("DOMContentLoaded",function() {
-  const buttons=document.querySelectorAll('[data-tab-button]')
- 
-  for (let index = 0; index < buttons.length; index++) {
-       buttons[index].addEventListener('click',function(botao) {
-        abaAlvo=botao.target.dataset.tabButton
-        aba=document.querySelector(`[data-tab-id=${abaAlvo}]`)
-        escondeAbas()
-        aba.classList.add('shows__list--is-active')
-        removeBotaoAtivo()
-        botao.target.classList.add("shows__tabs__buttons--is-active")
+document.addEventListener("DOMContentLoaded", function () {
+  const buttons = document.querySelectorAll("[data-tab-button]");
 
-       })
-    
+  for (let index = 0; index < buttons.length; index++) {
+    buttons[index].addEventListener("click", function (botao) {
+      abaAlvo = botao.target.dataset.tabButton;
+      aba = document.querySelector(`[data-tab-id=${abaAlvo}]`);
+      escondeAbas();
+      aba.classList.add("shows__list--is-active");
+      removeBotaoAtivo();
+      botao.target.classList.add("shows__tabs__buttons--is-active");
+    });
   }
-})
+});
 
 function removeBotaoAtivo() {
-  const buttons=document.querySelectorAll('[data-tab-button]')
-for (let index = 0; index < buttons.length; index++) {
-buttons[index].classList.remove('shows__tabs__buttons--is-active')  
-}
+  const buttons = document.querySelectorAll("[data-tab-button]");
+  for (let index = 0; index < buttons.length; index++) {
+    buttons[index].classList.remove("shows__tabs__buttons--is-active");
+  }
 }
 
-function escondeAbas(){
-  ulsContainer=document.querySelectorAll('[data-tab-id]')
+function escondeAbas() {
+  ulsContainer = document.querySelectorAll("[data-tab-id]");
   for (let index = 0; index < ulsContainer.length; index++) {
-     ulsContainer[index].classList.remove('shows__list--is-active')    
+    ulsContainer[index].classList.remove("shows__list--is-active");
   }
 }
