463b5a2ddee4922abf9336e68d0a43196e373daf
Matheus Viersa <matheusviersa995@hotmail.com>
2024-05-06 11:57:02 -0300

    Projeto finalizado

--- src/main.js
+++ src/main.js
@@ -1,5 +1,18 @@
 document.addEventListener("DOMContentLoaded", function () {
   const buttons = document.querySelectorAll("[data-tab-button]");
+  const questions=document.querySelectorAll("[data-faq-question]")
+
+  const heroSection=document.querySelectorAll('.hero')
+  const alturaHero=heroSection.clientHeight
+
+  window.addEventListener('scroll',function() {
+   const posicaoAtual=this.window.scrollY
+   if (posicaoAtual < alturaHero) {
+    ocultaElementos()
+   }else{
+    exibeElementos()
+   }
+  })
 
   for (let index = 0; index < buttons.length; index++) {
     buttons[index].addEventListener("click", function (botao) {
@@ -11,13 +24,32 @@ document.addEventListener("DOMContentLoaded", function () {
       botao.target.classList.add("shows__tabs__buttons--is-active");
     });
   }
+  for (let index = 0; index < questions.length; index++) {
+    questions[index].addEventListener('click',abreOuFecha)
+   }
 });
+
+function abreOuFecha(elemento){
+  const classe='faq__questions__item--is-open'
+ const elementoPai=elemento.target.parentNode
+ elementoPai.classList.toggle(classe)
+}
 
 function removeBotaoAtivo() {
   const buttons = document.querySelectorAll("[data-tab-button]");
   for (let index = 0; index < buttons.length; index++) {
     buttons[index].classList.remove("shows__tabs__buttons--is-active");
   }
+}
+
+function ocultaElementos() {
+  const header=document.querySelector('header')
+  header.classList.add("header--is-hidden")
+}
+
+function exibeElementos() {
+  const header=document.querySelector('header')
+  header.classList.remove("header--is-hidden")
 }
 
 function escondeAbas() {
